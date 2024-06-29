c48937cf09b839eed2d07f2bd1decb6ca4055f8b
guntsv <guntsvzz@gmail.com>
2024-06-29 15:18:28 +0700

    Update clone_to_mcr.sh

--- 02-GRECS/clone_to_mcr.sh
+++ 02-GRECS/clone_to_mcr.sh
@@ -10,9 +10,9 @@ mkdir -p ../03-UNICORN/tmp/clothing
 mkdir -p ../03-UNICORN/tmp/cellphones
 
 # Copy the directories to the new locations
-cp -r data/beauty/Amazon_Beauty_01_01/ ../03-UNICORN/tmp/beauty
-cp -r data/cds/Amazon_CDs_01_01/ ../03-UNICORN/tmp/cds
-cp -r data/clothing/Amazon_Clothing_01_01/ ../03-UNICORN/tmp/clothing
-cp -r data/cellphones/Amazon_Cellphones_01_01/ ../03-UNICORN/tmp/cellphones
+cp -r data/beauty/Amazon_Beauty_01_01/* ../03-UNICORN/tmp/beauty
+cp -r data/cds/Amazon_CDs_01_01/* ../03-UNICORN/tmp/cds
+cp -r data/clothing/Amazon_Clothing_01_01/* ../03-UNICORN/tmp/clothing
+cp -r data/cellphones/Amazon_Cellphones_01_01/* ../03-UNICORN/tmp/cellphones
 
 echo "Cloning complete."
